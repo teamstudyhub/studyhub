f5658a63c457d290dcbc11e7b5cb625b2e898ecc
tgo46084 <tgo46084@gmail>
2023-06-09 16:21:43 +0530

    supabase update

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,10 +1,7 @@
-
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "../types/supabase";
-
-
 
 const supa_url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supa_key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const SupaClient = createClient<Database>(supa_url,supa_key);
+export const SupaClient = createClient<Database>(supa_url, supa_key);
